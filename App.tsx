9d557fafbfc9ade478654058918689540014def6
adam <allpyjs@gmail.com>
2024-10-23 10:46:15 -0700

    release version

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { SafeAreaView, StatusBar, View } from "react-native";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
+import Header from "./src/components/Header";
+import imageState from "./src/state/imageState";
+import Body from "./src/components/Body";
+import suggestionState from "./src/state/suggestionState";
+import { fetchImages, fetchSuggestion } from "./src/fetchData";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import internetState from "./src/state/internetState";
+import NetInfo from "@react-native-community/netinfo";
+
+const AppContent = () => {
+  const setImages = useSetRecoilState(imageState);
+  const setSuggestion = useSetRecoilState(suggestionState);
+  const [hasInternet, setHasInternet] = useRecoilState(internetState);
+
+  useEffect(() => {
+    // Internet check
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      console.log(`Internet state: ${state.type} ${state.isInternetReachable}`);
+      setHasInternet(state.isInternetReachable);
+    });
+
+    // Fetch getImages and prompt suggestion
+    (async () => {
+      const images = await fetchImages();
+      const suggestion = await fetchSuggestion();
+
+      setImages(images ?? []); // Provide an empty array as the default value
+      setSuggestion(suggestion ?? ""); // Provide an empty string as the default value
+    })();
+
+    return () => {
+      unsubscribe(); // Stop internet connection check on unmount
+    };
+  }, []);
+
+  return (
+    <View className="flex-1">
+      <SafeAreaView />
+      <Header />
+      <Body />
+      <StatusBar barStyle={"dark-content"} />
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      <AppContent />
+      <Toast />
+    </RecoilRoot>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Linking, TouchableOpacity, Image } from "react-native";
+import openURL from "../lib/Util";
+import { WEBSITE } from "@env";
+
+const Header = () => {
+  const gitHubIcon = require("../images/25231.png");
+
+  return (
+    <View className="flex-row bg-white justify-between items-center px-4 py-2">
+      <View>
+        <Text className="text-xl font-bold">
+          Image Generator by{" "}
+          <Text
+            onPress={() => openURL(WEBSITE)}
+            className="underline font-extrabold text-blue-700"
+          >
+            Jlee
+          </Text>
+        </Text>
+        <Text className="text-gray-500">Powered by OpenAI & Azure</Text>
+      </View>
+      <TouchableOpacity
+        onPress={() =>
+          openURL("https://github.com/allpyjs/Image-Generation-Mobile-App")
+        }
+      >
+        <Image source={gitHubIcon} style={{ width: 24, height: 24 }} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Header;
